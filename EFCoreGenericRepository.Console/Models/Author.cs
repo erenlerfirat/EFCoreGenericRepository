eebbc0539c2219e2a4a031a10a81751583231740
erenlerfirat <73332309+erenlerfirat@users.noreply.github.com>
2023-02-19 15:26:08 +0300

    Models add

---
+++ EFCoreGenericRepository.Console/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿namespace EFCoreGenericRepository.Console.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Book> Books { get; set; }
+        public Author()
+        {
+            Books= new List<Book>();
+            Name = string.Empty;
+            Age = 0;
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
---
+++ EFCoreGenericRepository.Console/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreGenericRepository.Console.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
